abdfc74073c42e543d905f3b51a2dee023fab8d6
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2025-07-29 09:28:57 -0500

    skills

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const pro = [{"nombre": "Proyecto 1", "descripción": "Descripción", "lenguajes
     {"nombre": "Proyecto 1", "descripción": "Descripción", "lenguajes": "Lenguajes", "portada": "img/foto.jpg" },
     {"nombre": "Proyecto 1", "descripción": "Descripción", "lenguajes": "Lenguajes", "portada": "img/foto.jpg" }
 ];
+const skills = [{"nombre": "HTML", "imagen": "./img/html.png" },
+    {"nombre": "JS", "imagen": "./img/js.png" },
+    {"nombre": "PHP", "imagen": "./img/php.png" },
+    {"nombre": "JAVA", "imagen": "./img/java.png" },
+    {"nombre": "NODE JS", "imagen": "./img/node.png" },
+    {"nombre": "JOOMLA", "imagen": "./img/joomla.png" },
+    {"nombre": "GANTRY", "imagen": "./img/gantry.png" },
+    {"nombre": "MYSQL", "imagen": "./img/mysql.png" },
+    {"nombre": "POSTGRESQL", "imagen": "./img/postgres.png" }];
+
 cargarProyectos();
+cargarSkills();
 
 function cargarProyectos() {
     const div = document.getElementById('projects');
     for (let i = 0; i < pro.length; i++) {
-        div.innerHTML += `<div class="project" id="project_${i}" title="Detalles"><img src="${pro[i].portada}" alt="imagen del proyecto"></div>`;
+        div.innerHTML += `<div class="project" id="project_${i}" title="Detalles"><img src="${pro[i].portada}" alt="imagen del proyecto"><span>${pro[i].nombre}</span></div>`;
+    }
+}
+
+function cargarSkills() {
+    const div = document.getElementById('skills');
+    for (let i = 0; i < skills.length; i++) {
+        div.innerHTML += `<div class="skill" id="skill_${i}" title="${skills[i].nombre}"><img src="${skills[i].imagen}" alt="imagen de ${skills[i].nombre}"></div>`;
     }
 }
 
